cd91c213e03689df840afad28be4f94224003c4e
planetlucid <planetlucid@gmail.com>
2020-02-11 20:33:30 -0500

    saved tables

--- db/migrate/20200212011801_create_teams.rb
+++ db/migrate/20200212011801_create_teams.rb
@@ -1,6 +1,9 @@
 class CreateTeams < ActiveRecord::Migration[6.0]
   def change
     create_table :teams do |t|
+    t.belongs_to :hero
+    t.belongs_to :comic
+    t.string :name
     end
   end
 end
--- db/migrate/20200212012133_create_heros.rb
+++ db/migrate/20200212012133_create_heros.rb
@@ -1,6 +1,7 @@
 class CreateHeros < ActiveRecord::Migration[6.0]
   def change
     create_table :heros do |t|
+      t.integer :power
     end
   end
 end
