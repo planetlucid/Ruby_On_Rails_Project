d3c77265a6ae83d316f4ec36d44a93f8088fd371
planetlucid <planetlucid@gmail.com>
2020-02-13 12:11:53 -0500

    log in log out 2923

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+  #this one line sends us to the log in page
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def welcome
+  end
+
+  #protected is a stage bedtween private and public 
+  #and means that child classes can use this method
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
-
-
-#First Commit
