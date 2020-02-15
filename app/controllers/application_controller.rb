a7971d3eba599c22ba96fe0ca543a2f64db56794
github username <github email address>
2020-02-15 13:17:20 -0500

    continue with Omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
   def welcome
+    byebug
   end
 
   #protected is a stage bedtween private and public 
