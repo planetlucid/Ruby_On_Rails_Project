7d5ac76114fb74ee074e04c17da79123123e0ad0
github username <github email address>
2020-02-15 14:29:01 -0500

    sign in with email or github

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
   def welcome
-    byebug
+    #byebug
   end
 
   #protected is a stage bedtween private and public 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[github]
 
          validates :username, presence: true
+
+         def self.from_omniauth(auth)
+          where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+            user.email = auth.info.email
+            user.username = auth.info.nickname
+            user.password = Devise.friendly_token[0, 20]
+          end
+        end
 end
